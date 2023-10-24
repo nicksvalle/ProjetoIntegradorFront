24af7b0a1c55feebd2e040220836b34b124fe661
nicksvalle <nicolasdeoliveiravalle@gmail.com>
2023-10-24 11:27:50 -0300

    primeiro de muitos

---
+++ src/app/coordenador-portal/coordenador-portal.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CoordenadorPortalComponent } from './coordenador-portal.component';
+
+describe('CoordenadorPortalComponent', () => {
+  let component: CoordenadorPortalComponent;
+  let fixture: ComponentFixture<CoordenadorPortalComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CoordenadorPortalComponent]
+    });
+    fixture = TestBed.createComponent(CoordenadorPortalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/coordenador-portal/coordenador-portal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coordenador-portal',
+  templateUrl: './coordenador-portal.component.html',
+  styleUrls: ['./coordenador-portal.component.css']
+})
+export class CoordenadorPortalComponent {
+
+}
